c9393e1de1e481aaef12372a77b843cb3c54b04a
cancer <zodiac.cancer.j6@gmail.com>
2025-05-20 16:26:00 +0900

    Add demo for 1.md

---
+++ src/routes/1/+page.svelte
@@ -0,0 +1,17 @@
+<script>
+let count = $state(0);
+
+function onClickButton () {
+  count += 1;
+}
+</script>
+
+<div>
+  <output>{count}</output>
+</div>
+<button onclick={onClickButton}>button</button>
+
+<style>
+/* 必要なければstyleタグごと消してよい */
+</style>
+
