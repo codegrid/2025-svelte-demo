1cda7400d9e0be8c30e8ce7ea7b262623c788e25
cancer <zodiac.cancer.j6@gmail.com>
2025-06-20 13:29:17 +0900

    Add demo for 3-1

---
+++ src/routes/3/1/+page.svelte
@@ -0,0 +1,14 @@
+<script>
+  import Header from "./Header.svelte";
+
+  const title = "Svelte";
+  let suffix = $state("😀");
+
+  const changeSuffix = () => {
+    const emojis = ["😀", "🔥", "🌊", "🌲", "💰", "⛰️", "😎"];
+    suffix = emojis.at(Math.floor(Math.random() * emojis.length));
+  };
+</script>
+
+<Header {title} {suffix} />
+<button onclick={changeSuffix}>Change Emoji</button>
