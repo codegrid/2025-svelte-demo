9f38d8c2496f38305f61522edbe3a0f6af32f079
cancer <zodiac.cancer.j6@gmail.com>
2025-07-29 16:51:57 +0900

    Add demo for 5-2

---
+++ src/routes/5/2/+page.svelte
@@ -0,0 +1,27 @@
+<script>
+  let brothers = [
+    {
+      id: 'ichiro',
+      name: '一郎',
+      age: 20,
+    },
+    {
+      id: 'jiro',
+      name: '二郎',
+      age: 18,
+    },
+    {
+      id: 'saburo',
+      name: '三郎',
+      age: 12,
+    },
+  ]
+</script>
+
+<ul>
+  {#each brothers as bro, index (bro.id)}
+    <li>{index + 1}: {bro.name}（{bro.age}歳）</li>
+  {:else}
+    <li>兄弟はいません。</li>
+  {/each}
+</ul>
