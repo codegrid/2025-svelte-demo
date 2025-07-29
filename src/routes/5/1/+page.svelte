283925dcded44ab2e9096d45012b05ccc873d2a9
cancer <zodiac.cancer.j6@gmail.com>
2025-07-29 16:51:48 +0900

    Add demo for 5-1

---
+++ src/routes/5/1/+page.svelte
@@ -0,0 +1,25 @@
+<script>
+  let payment = $state("card");
+
+  const onChangeSelect = (event) => {
+    payment = event.target.value;
+  };
+</script>
+
+<form>
+  <select onchange={onChangeSelect}>
+    <option value="card">クレジットカード</option>
+    <option value="digital_currency">電子マネー</option>
+    <option value="others">その他</option>
+  </select>
+</form>
+
+<div>
+  {#if payment === "card"}
+    <p>クレジットカードの設定にお進みください。</p>
+  {:else if payment === "digital_currency"}
+    <p>電子マネーを選択してください。</p>
+  {:else}
+    <p>お問い合わせください。</p>
+  {/if}
+</div>
