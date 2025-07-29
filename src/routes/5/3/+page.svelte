e2137ed155cfe53dc04edf6f933c93fc69275f51
cancer <zodiac.cancer.j6@gmail.com>
2025-07-29 16:52:04 +0900

    Add demo for 5-3

---
+++ src/routes/5/3/+page.svelte
@@ -0,0 +1,11 @@
+<script>
+  let brothers = [];
+</script>
+
+<ul>
+  {#each brothers as bro, index (bro.id)}
+    <li>{index}: {bro.name}（{bro.age}歳）</li>
+  {:else}
+    <li>兄弟はいません。</li>
+  {/each}
+</ul>
